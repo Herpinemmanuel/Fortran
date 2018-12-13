f08d146112f35443828b354b9274cf8760321f3a
Herpin Emmanuel <herpin.emmanuel@gmail.com>
2018-12-13 22:36:23 +0100

    Add files via upload

---
+++ 16_Ecriture_dans_un_fichier_texte.f90
@@ -0,0 +1,22 @@
+program prog3
+	implicit none
+	real :: dt, c, alpha
+	integer :: Nx, Nb_iter
+	open(UNIT=10,FILE='param.dat',STATUS="unknown",POSITION="append")
+	write(*,*) 'Donner la valeur de Nx'
+	read(*,*) Nx
+	write(10,*) Nx
+	write(*,*) 'Donner la valeur de dt'
+	read (*,*) dt
+	write(10,*) dt
+	write(*,*) 'Donner la valeur de c'
+	read (*,*) c
+	write(10,*) c
+	write(*,*) 'Donner la valeur de alpha'
+	read (*,*) alpha
+	write(10,*) alpha
+	write(*,*) 'Donner la valeur de Nb_iter'
+	read (*,*) Nb_iter
+	write(10,*) Nb_iter
+
+end program
